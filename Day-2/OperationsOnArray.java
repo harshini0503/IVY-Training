7b4dc317c1931da067b52253a54994745217d1b2
harshini0503 <47024269+harshini0503@users.noreply.github.com>
2022-08-09 13:33:02 +0530

    Calculated all mean, mode and median of an array

--- Day-2/OperationsOnArray.java
+++ Day-2/OperationsOnArray.java
@@ -9,21 +9,47 @@ public class OperationsOnArray {
                 for(int i=0;i<length;i++)
                     array[i]=sc.nextInt();
                 float total=0;
-                float mean=0;
+                float mean,median=0;
+
+                //mean
+        
                 for(int i=0; i<length; i++)
                 {
                     total = total+array[i];
                 }
                 mean = total/length;
                 System.out.println("The mean value is: "+mean);
-         System.out.println("---------------------------");
-                Arrays.sort(array);
-                if(length%2==0)
-                    median= array[length/2];
-                else
-                    median=(array[(length - 1) / 2] + array[length / 2]) / 2;
-
-                System.out.println(" The median is :" +median);
         System.out.println("---------------------------");
+        //median
+        
+        Arrays.sort(array);
+        if(length%2==0)
+            median= array[length/2];
+        else
+            median=(array[(length -1) / 2] + array[length / 2]) / 2;
+
+            System.out.println(" The median is :" +median);
+        System.out.println("---------------------------");
+        
+        //mode
+        
+        int maxVal=0, count=0, maxCount=0;
+
+        for(int i=0;i<length;i++) {
+            count = 0;
+            for(int j=0;j<length;j++) {
+                if(array[i] == array[j]) {
+                    count++;
+                }
+            }
+            if (count > maxCount) {
+                maxCount = count;
+                maxVal = array[i];
+            }
+        }
+                System.out.println("\nThe mode is : "+maxVal);
+
+        System.out.println("---------------------------");
+
     }
     }
